6fb7b1f6416d92377ac918ef745e1c02c24a1007
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2023-10-12 10:37:06 +0800

    :sparkles: getRemoteHost、getHeaderNames

---
+++ src/main/java/cn/com/xuxiaowei/java/controller/IndexRestController.java
@@ -0,0 +1,40 @@
+package cn.com.xuxiaowei.java.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+@RestController
+public class IndexRestController {
+
+    @RequestMapping("")
+    public Map<String, Object> index(HttpServletRequest request, HttpServletResponse response) {
+        Map<String, Object> map = new HashMap<>();
+        Map<String, Object> headerNamesMap = new HashMap<>();
+        map.put("headerNames", headerNamesMap);
+
+        String remoteHost = request.getRemoteHost();
+        map.put("remoteHost", remoteHost);
+
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            Enumeration<String> headers = request.getHeaders(headerName);
+
+            headerNamesMap.put(headerName, Collections.list(headers));
+        }
+
+        return map;
+    }
+
+}
