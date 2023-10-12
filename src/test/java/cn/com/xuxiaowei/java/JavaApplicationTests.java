0d0f281af29a496362bb45b4c4717057b6c770ce
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2023-10-12 09:26:45 +0800

    :tada: init

---
+++ src/test/java/cn/com/xuxiaowei/java/JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
